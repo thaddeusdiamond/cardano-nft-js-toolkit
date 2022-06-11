e7d69adfa1b640eca9435c4cc3758fa01337873b
Thaddeus Diamond <thaddeus.diamond@gmail.com>
2022-06-10 21:06:17 -0500

    webpack: Stylistic fix for plugin provider list
    Remove duplicate declarations of this class in webpack.config.cjs.
    
    [ Testing: npm run build ]

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -21,10 +21,7 @@ module.exports = {
         // Work around for Buffer is undefined:
         // https://github.com/webpack/changelog-v5/issues/10
         new webpack.ProvidePlugin({
-            Buffer: ['buffer', 'Buffer'],
-        }),
-        new webpack.ProvidePlugin({
-            process: 'process/browser',
+            Buffer: ['buffer', 'Buffer']
         }),
 
         new copyplugin({
