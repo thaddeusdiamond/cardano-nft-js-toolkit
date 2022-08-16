f0a62ddea2c80d9a8c097e8427eac5ff2dc9cb05
Thaddeus Diamond <thaddeus.diamond@gmail.com>
2022-08-16 12:33:13 -0500

    Sweep: Prototype for Sweep by WildTangz
    THis is a prototype for the message-receiving implementation of Sweep by
    Wild Tangz.  More information on this architecture can be found in the
    Snipe repository.
    
    The key functionality here includes:
    * Validating that the user holds the required number of Tangz to use the
      browser tool
    * Retrieving available UTxOs from a user's wallet and passing to the
      extension
    * Executing transactions that are presented to the user
    
    [ Documentation: None ]
    [ Testing: End-to-end testing in browser ]

--- src/js/library.js
+++ src/js/library.js
@@ -7,4 +7,5 @@ export * as DropPayment from "./pages/drop-payment.js";
 export * as BulkPayment from "./pages/bulk-payment.js";
 export * as NftCreator from "./pages/nft-creator.js";
 export * as SniperBot from "./pages/sniper-bot.js";
+export * as Sweep from "./pages/sweep.js";
 export * as VendingMachine from "./pages/vending-machine.js";
---
+++ src/js/pages/sweep.js
@@ -0,0 +1,139 @@
+import * as CardanoDAppJs from "../third-party/cardano-dapp-js.js";
+import * as LucidInst from "../third-party/lucid-inst.js";
+
+import {coreToUtxo, fromHex, C as LCore, TxComplete} from "lucid-cardano";
+import {shortToast} from "../third-party/toastify-utils.js";
+
+const MSG_ID = '674';
+const MSG_KEY = 'msg';
+const MAX_METADATA_LEN = 64;
+
+function numRequiredPolicyAssets(requiredPolicy, availableUtxos) {
+  const requiredAssets = {};
+  for (const availableUtxo of availableUtxos) {
+    const assets = availableUtxo.assets;
+    for (const asset in assets) {
+      if (asset.startsWith(requiredPolicy)) {
+        requiredAssets[asset] = assets[asset];
+      }
+    }
+  }
+  return Object.values(requiredAssets).reduce((acc, amount) => acc + amount, 0n);
+}
+
+async function cardanoDAppWallet() {
+  var cardanoDApp = CardanoDAppJs.getCardanoDAppInstance();
+  if (!cardanoDApp.isWalletConnected()) {
+    throw 'Please connect a wallet before sweeping using "Connect Wallet" button';
+  }
+  return await cardanoDApp.getConnectedWallet();
+}
+
+async function connectedLucidInst(blockfrostKey, wallet) {
+  const lucid = await LucidInst.getLucidInstance(blockfrostKey);
+  if (lucid === undefined) {
+    throw 'Please validate that your wallet is on mainnet';
+  }
+  if (lucid.network !== 'Mainnet') {
+    throw 'Internal error (NETWORK) please contact devs';
+  }
+  lucid.selectWallet(wallet);
+  return lucid;
+}
+
+async function validateHoldings(lucid, requiredPolicy, minAssets) {
+  const address = await lucid.wallet.address();
+  const availableUtxos = await lucid.wallet.getUtxos();
+  const numPolicyAssets = numRequiredPolicyAssets(requiredPolicy, availableUtxos);
+  if (numPolicyAssets < minAssets) {
+    throw `The sweeper currently requires ${minAssets} NFTs with policy ID ${requiredPolicy} to be used!`;
+  }
+}
+
+async function getWalletInfo(wallet, lucid) {
+  return {
+    address: await lucid.wallet.address(),
+    collateral: await wallet.experimental.getCollateral(),
+    utxos: await wallet.getUtxos()
+  }
+}
+
+async function executeCborTxn(lucid, txn) {
+  const txnBytes = fromHex(txn.cbor);
+  const txnFromCbor = LCore.Transaction.from_bytes(txnBytes);
+  var txComplete = new TxComplete(lucid, txnFromCbor);
+  var txSigned = await txComplete.sign().complete();
+  return await txSigned.submit();
+}
+
+async function executePayToTxn(lucid, txn) {
+  const receiptMetadata = {
+    MSG_KEY: txn.receipt.match(new RegExp(`.{1,${MAX_METADATA_LEN}}`, 'g'))
+  }
+  const lucidUtxos = txn.utxos.map(utxo => {
+    const utxoBytes = fromHex(utxo);
+    const utxoCore = LCore.TransactionUnspentOutput.from_bytes(utxoBytes);
+    return coreToUtxo(utxoCore);
+  });
+
+  const txComplete = await lucid.newTx()
+                                .collectFrom(lucidUtxos)
+                                .attachMetadata(MSG_ID, receiptMetadata)
+                                .payToAddress(txn.address, {lovelace: txn.amount})
+                                .complete();
+  const txSigned = await txComplete.sign().complete();
+  return await txSigned.submit();
+}
+
+async function executeTxn(lucid, txn) {
+  if (txn.type === 'pay_to_address') {
+    return executePayToTxn(lucid, txn);
+  } else if ('cbor' in txn) {
+    return executeCborTxn(lucid, txn);
+  }
+  throw `Unrecognized txn: ${txn}`;
+}
+
+export async function processMessageData(message) {
+  switch (message.type) {
+    case "WT_START_SWEEP":
+      try {
+        const wallet = await cardanoDAppWallet();
+        const lucid = await connectedLucidInst(message.params.blockfrostKey, wallet);
+        await validateHoldings(lucid, message.params.requiredPolicy, message.params.minAssets);
+        const walletInfo = await getWalletInfo(wallet, lucid);
+        window.postMessage({ type: "WT_WALLET_READY", wallet: walletInfo }, "*");
+      } catch (err) {
+        window.postMessage({ type: "WT_WALLET_ERROR", err: err }, "*");
+      }
+      break;
+    case "WT_PERFORM_TXNS":
+      var completedTxns = 0;
+      const wallet = await cardanoDAppWallet();
+      const lucid = await connectedLucidInst(message.params.blockfrostKey, wallet);
+      try {
+        const feeTxHash = await executeTxn(lucid, message.params.feeTxn);
+        shortToast(`Thank you for paying the fee! Confirmation of tx: ${feeTxHash}`);
+        for (const txn of message.params.txns) {
+          try {
+            const txHash = await executeTxn(lucid, txn);
+            shortToast(`Successfully sent sweep tx: ${txHash}!`);
+            completedTxns += 1;
+          } catch (err) {
+            const errMsg = (typeof err === 'string') ? err : JSON.stringify(err);
+            if (!confirm(`Transaction cancelled or failed, would you like to continue? (${errMsg})`)) {
+              break;
+            }
+          }
+        }
+        alert('Sweep completed successfully.  Please refresh or refocus the page as the items below may be showing inaccurately.  Check your wallet to ensure that items marked "UNAVAILABLE" below were successfully purchased.');
+      } catch {
+        shortToast(`Unsuccessful fee transaction, aborting sweep`);
+      }
+      window.postMessage({ type: "WT_SWEEP_COMPLETE", completed: completedTxns });
+      break;
+    default:
+      //console.log(`Received unknown message of type ${message.type}: ${message.text}`);
+      return;
+  }
+}
