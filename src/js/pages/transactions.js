fb806d53d1cf43d4f1e149635345f3d003d43d00
Thaddeus Diamond <thaddeus.diamond@gmail.com>
2023-01-14 23:20:40 -0600

    Transactions: Add the ability to pull across only a specific range and support handles
    Note that there was an issue here getting the blocks from a specific
    slot enclosed in the block (the blockfrost API appears to want only
    specific slot endpoints), so we simply omit that for now, the behavior
    is the same, though sub-optimal.
    
    [ Testing: Manual QA ]

--- src/js/pages/transactions.js
+++ src/js/pages/transactions.js
@@ -1,11 +1,26 @@
+import * as CardanoDAppJs from "../third-party/cardano-dapp-js.js";
 import * as LucidInst from "../third-party/lucid-inst.js";
 
 import {longToast} from "../third-party/toastify-utils.js";
+import {validate, validated} from "../nft-toolkit/utils.js";
 
-import {getAddressDetails} from "lucid-cardano";
+import {getAddressDetails, toHex} from "lucid-cardano";
 
-const RETRIES = 3;
+const NEWEST_BLOCK = 2147483647;  // maxint
+const OLDEST_BLOCK = 1;
+
+const HANDLE_IDENTIFIER = '$';
+const HANDLE_POLICY = 'f0ff48bbb7bbe9d59a40f1ce90e9e9d0ff5002ec48f232b49ca0fb9a';
 const STAKE_PREFIX = 'stake';
+
+const RETRY_DELAY = 5000;
+const RETRIES = 3;
+
+const AUTHORIZATION_MINS = {
+    "b000e9f3994de3226577b4d61280994e53c07948c8839d628f4a425a" : 3,
+    "33568ad11f93b3e79ae8dee5ad928ded72adcea719e92108caf1521b" : 3,
+    "33566617519280305e147975f80914cea1c93e8049567829f7370fca" : 1
+}
 
 async function callBlockfrost(endpoint, blockfrostKey) {
   const blockfrostSettings = await LucidInst.getBlockfrostParams(blockfrostKey);
@@ -21,9 +36,10 @@ async function callBlockfrost(endpoint, blockfrostKey) {
       return result;
     } catch (err) {
       mostRecentError = err;
+      await (new Promise(resolveFunc => setTimeout(resolveFunc, RETRY_DELAY)));
     }
   }
-  throw mostRecentError;
+  throw `Unrecoverable Blockfrost error: ${mostRecentError}`;
 }
 
 async function* callPaginatedBlockfrost(endpoint, paramsString, blockfrostKey) {
@@ -40,30 +56,65 @@ async function* callPaginatedBlockfrost(endpoint, paramsString, blockfrostKey) {
   }
 }
 
+async function locationOfHandle(handle, blockfrostApiKey) {
+  const handleHex = toHex(new TextEncoder('UTF-8').encode(handle.toLowerCase().slice(HANDLE_IDENTIFIER.length)));
+  const handleAddresses = await callBlockfrost(`assets/${HANDLE_POLICY}${handleHex}/addresses`, blockfrostApiKey);
+  validate(handleAddresses.length === 1, 'Invalid state: handle found at multiple locations');
+  return handleAddresses[0].address;
+}
+
 async function determineAccountOf(walletAddr, blockfrostApiKey) {
   if (walletAddr.startsWith(STAKE_PREFIX)) {
     return walletAddr;
   }
   const lucid = await LucidInst.getLucidInstance(blockfrostApiKey);
-  const addrDetails = getAddressDetails(walletAddr);
+  const normalizedAddress = (walletAddr.startsWith(HANDLE_IDENTIFIER)) ? (await locationOfHandle(walletAddr, blockfrostApiKey)) : walletAddr;
+  const addrDetails = getAddressDetails(normalizedAddress);
   if (addrDetails.stakeCredential === undefined) {
     return undefined;
   }
   return lucid.utils.credentialToRewardAddress(addrDetails.stakeCredential);
 }
 
-export async function getHistoryOf(walletAddr, blockfrostApiKey, txnCallbackFunc) {
-  const stakeAddress = await determineAccountOf(walletAddr, blockfrostApiKey);
-  if (stakeAddress === undefined) {
-    longToast('Unstaked addresses are currently unsupported');
+async function getBlockFor(unixTime, lucid, blockfrostApiKey) {
+  if (!unixTime) {
+    return undefined;
   }
-  const associatedAddresses = callPaginatedBlockfrost(`accounts/${stakeAddress}/addresses`, '', blockfrostApiKey);
-  for await (const address of associatedAddresses) {
-    const transactions = callPaginatedBlockfrost(`addresses/${address.address}/transactions`, 'order=desc', blockfrostApiKey);
-    for await (const transaction of transactions) {
-      const transactionAmounts = await callBlockfrost(`txs/${transaction.tx_hash}/utxos`, blockfrostApiKey);
-      await txnCallbackFunc(transaction, transactionAmounts);
+  const slot = lucid.utils.unixTimeToSlot(unixTime);
+  return await callBlockfrost(`blocks/slot/${slot}`, blockfrostApiKey).height;
+}
+
+export async function getHistoryOf(walletAddr, blockfrostApiKey, startTime, endTime, txnCallbackFunc) {
+  try {
+    validate(walletAddr, 'Please enter a wallet handle before clicking "Load"');
+
+    const cardanoDApp = CardanoDAppJs.getCardanoDAppInstance();
+    validate(cardanoDApp.isWalletConnected(), 'Please connect a wallet before retrieving transactions using "Connect Wallet" button');
+
+    const lucid = await LucidInst.getLucidInstance(blockfrostApiKey);
+    validate(lucid, 'Your blockfrost key does not match the network of your wallet.');
+
+    const startTimeBlock = OLDEST_BLOCK; //(await getBlockFor(startTime, lucid, blockfrostApiKey)) || OLDEST_BLOCK;
+    const endTimeBlock = NEWEST_BLOCK; //(await getBlockFor(endTime, lucid, blockfrostApiKey)) || NEWEST_BLOCK;
+
+    validate(await cardanoDApp.walletMeetsTokenGate(AUTHORIZATION_MINS), 'At least 3 Wild Tangz or 1 Buffoon is required to use this software, please purchase and try again.');
+
+    const stakeAddress = validated(await determineAccountOf(walletAddr, blockfrostApiKey), 'Unstaked addresses are currently unsupported');
+    const associatedAddresses = await callPaginatedBlockfrost(`accounts/${stakeAddress}/addresses`, '', blockfrostApiKey);
+    for await (const address of associatedAddresses) {
+      const params = ['order=desc', `from=${startTimeBlock}`, `to=${endTimeBlock}`].join('&')
+      const transactions = await callPaginatedBlockfrost(`addresses/${address.address}/transactions`, params, blockfrostApiKey);
+      for await (const transaction of transactions) {
+        const blockUnixTime = transaction.block_time * 1000;
+        if ((startTime && startTime > blockUnixTime) || (endTime && endTime < blockUnixTime)) {
+          continue;
+        }
+        const transactionAmounts = await callBlockfrost(`txs/${transaction.tx_hash}/utxos`, blockfrostApiKey);
+        await txnCallbackFunc(transaction, transactionAmounts, stakeAddress);
+      }
     }
+  } catch (err) {
+    longToast(err);
   }
 }
 
@@ -90,4 +141,10 @@ export async function calculateNetAmounts(transactionAmounts, blockfrostApiKey) 
     await updateNetAmountsFor(netAmounts, output, +1, blockfrostApiKey);
   }
   return netAmounts;
+}
+
+export async function retrieveDailyHighs(crypto, currency, marketplace) {
+  // TODO: Change this to a proxy server
+  //return await fetch(`https://api.cryptowat.ch/markets/${marketplace}/${crypto}${currency}/ohlc`);
+  return {}
 }
